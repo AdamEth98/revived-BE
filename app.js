4175245b123b143fa69fb93f7a6cb1aee43ef25c
AdamEth98 <adam.etherington1@outlook.com>
2022-08-09 11:23:14 +0100

    begins auth implementation

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ const userSchema = require("./schemas/User");
 require("dotenv").config();
 const db = require("./utils/db");
 
+// auth requires
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+
 // app settings
 const app = express();
 const port = process.env.PORT || 3000;
+
+// auth settings
+app.use(
+  session({
+    secret: process.env.AUTH_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
