e60ca5315fc171a2fe048d0dbf234348b514204c
Lauren135 <laurengreenhill911@gmail.com>
2022-08-11 12:57:47 +0100

    changed charity schema 2

--- schemas/User.js
+++ schemas/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   password: { type: String, required: true },
   items: [],
-  charity: { type: String, default: null },
+  charity: { type: String, default: "" },
 });
 
 module.exports = mongoose.model("users", userSchema);
