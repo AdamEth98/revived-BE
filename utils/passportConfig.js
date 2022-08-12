6ffc95186c0997ad6ec2deec4b0b1dccb016e2e1
Lauren135 <laurengreenhill911@gmail.com>
2022-08-12 11:25:50 +0100

    fixed login

--- utils/passportConfig.js
+++ utils/passportConfig.js
@@ -4,20 +4,26 @@ const LocalStrategy = require("passport-local").Strategy;
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy((email, password, done) => {
-      userSchema.findOne({ email: email }, (err, user) => {
-        if (err) throw err;
-        if (!user) return done(null, false);
-        bcrypt.compare(password, user.password, (err, result) => {
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      (username, password, done) => {
+        userSchema.findOne({ email: username }, (err, user) => {
           if (err) throw err;
-          if (result === true) {
-            return done(null, user);
-          } else {
-            return done(null, false);
-          }
+          if (!user) return done(null, false);
+          bcrypt.compare(password, user.password, (err, result) => {
+            if (err) throw err;
+            if (result === true) {
+              return done(null, user);
+            } else {
+              return done(null, false);
+            }
+          });
         });
-      });
-    })
+      }
+    )
   );
   passport.serializeUser((user, cb) => {
     cb(null, user.id);
