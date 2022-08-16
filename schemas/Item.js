e08bee47f9ef76d5084303c31fd974601dc116f6
Lauren135 <laurengreenhill911@gmail.com>
2022-08-16 14:38:26 +0100

    changed item category

--- schemas/Item.js
+++ schemas/Item.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const itemSchema = new mongoose.Schema({
   itemname: { type: String, required: true },
   itemlocation: { type: String, required: true },
-  itemcategory: { type: String, required: true },
+  itemcategory: String,
   itemdescription: { type: String, default: "" },
   itemowner: String,
   itemownerid: { type: Schema.Types.ObjectId, ref: "itemownerid" },
